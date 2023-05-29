62ab2d1a0d2c61b566f0c9bbbd0f849d020d1ad4
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-05-30 02:56:26 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.11 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,16 +1,16 @@
+import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        int index = 0;
-        int[] hamburger = new int[ingredient.length];
+        Stack<Integer> hamburger = new Stack<>();
         for (int i = 0; i < ingredient.length; i++) {
-            hamburger[index] = ingredient[i];
-            index++;
-            if (index < 4) continue;
-            if (hamburger[index-4] == 1 && hamburger[index-3] == 2
-                    && hamburger[index-2] == 3 && hamburger[index-1] == 1) {
+            hamburger.push(ingredient[i]);
+            int size = hamburger.size();
+            if (size < 4) continue;
+            if (hamburger.get(size-4) == 1 && hamburger.get(size-3) == 2 
+                && hamburger.get(size-2) == 3 && hamburger.get(size-1) == 1) {
+                hamburger.pop(); hamburger.pop(); hamburger.pop(); hamburger.pop();
                 answer++;
-                index -= 4;
             }
         }
         return answer;
