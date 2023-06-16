02ba1a1beb0458c0a9ee4cb3809a45c76bdd6405
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-06-16 19:47:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -2,13 +2,10 @@ class Solution {
     public int solution(int left, int right) {
         int answer = 0;
         for (int i = left; i <= right; i++) {
-            int num = 0;
-            for (int j = 1; j <= Math.sqrt(i); j++) {
-                if (j * j == i) num++;
-                else if (i % j == 0) num = num + 2;
-            }
-            if (num % 2 == 0) answer += i;
-            else answer -= i;
+            //제곱수인 경우 약수의 개수가 홀수
+            if (i % Math.sqrt(i) == 0) answer -= i;
+            //제곱수가 아닌 경우 약수의 개수가 짝수
+            else answer += i;
         }
         return answer;
     }
