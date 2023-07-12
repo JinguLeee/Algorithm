55dc553dfa4e0bff7b7b9279b87d52448cc4e5bd
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-07-12 18:34:41 +0900

    [level 1] Title: 소수 만들기, Time: 0.37 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,19 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
+        Arrays.sort(nums);
+        int sum = nums.length - 1;
+        sum = nums[sum] + nums[sum-1] + nums[sum-2];
+        
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int i = 2; i <= sum; i++) numSet.add(i);
+        for (int i = 2; i <= sum/2; i++){
+            int num = i;
+            while ((num += i) <= sum) {
+                numSet.remove(num);
+            }
+        }
+
         int count = 0;
         for (int i = 0; i < nums.length-2; i++){
             for (int j = i+1; j < nums.length-1; j++){
                 for (int k = j+1; k< nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-
-                    for (int l = 2; l <= sum/2; l++){
-                        if (sum % l == 0) {
-                            count--;
-                            break;
-                        }
-                    }
-                    count++;
+                    if (numSet.contains(nums[i] + nums[j] + nums[k])) count++;
                 }
             }
         }
