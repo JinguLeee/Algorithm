ca961679ca3bf4bbcc5d3a0e455444527a88d914
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-07-15 17:22:50 +0900

    [Gold II] Title: 가운데를 말해요, Time: 484 ms, Memory: 34296 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) maxPq.offer(Integer.parseInt(br.readLine()));
+            else minPq.offer(Integer.parseInt(br.readLine()));
+
+            if (!minPq.isEmpty() && maxPq.peek() > minPq.peek()) {
+                int max = maxPq.poll();
+                maxPq.offer(minPq.poll());
+                minPq.offer(max);
+            }
+            sb.append(maxPq.peek()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
