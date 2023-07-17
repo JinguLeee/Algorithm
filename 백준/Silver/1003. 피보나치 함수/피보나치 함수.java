69425a41467e60e2f8abcf9b708beb829c4c3042
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-07-17 14:00:48 +0900

    [Silver III] Title: 피보나치 함수, Time: 148 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,19 +3,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, num[i]);
+        }
 
-        int[][] dp = new int[41][2];
+        max = (max < 1 ? 1 : max);
+        int[][] dp = new int[max+1][2];
         dp[0] = new int[] {1, 0};
         dp[1] = new int[] {0, 1};
-        for (int i = 2; i <= 40; i++) {
+        for (int i = 2; i <= max; i++) {
             dp[i][0] = dp[i-1][0] + dp[i-2][0];
             dp[i][1] = dp[i-1][1] + dp[i-2][1];
         }
-
+        
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            sb.append(dp[num][0] + " " + dp[num][1] + "\n");
+            sb.append(dp[num[i]][0] + " " + dp[num[i]][1] + "\n");
         }
         System.out.println(sb);
     }
