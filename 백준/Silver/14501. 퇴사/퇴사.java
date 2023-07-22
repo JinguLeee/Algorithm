855c10ae66a0dd21486c80374d7de9fdfb40e0a7
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-07-22 18:39:03 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] count = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            count[i][0] = Integer.parseInt(st.nextToken());
+            count[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        long[] dp = new long[N+1];
+        for (int i = N-1; i >= 0; i--) {
+            if (i + count[i][0] > N) dp[i] = dp[i+1];
+            else dp[i] = Math.max(dp[i+1], dp[i+count[i][0]] + count[i][1]);
+        }
+
+        System.out.println(dp[0]);
+    }
+}
