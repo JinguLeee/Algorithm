7f1e486ed8f80a900aabb148eda9dd9d13616283
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-07-27 20:45:12 +0900

    [Silver II] Title: 이동하기, Time: 536 ms, Memory: 78980 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N+1][M+1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N+1][M+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + arr[i][j];
+            }
+        }
+
+        System.out.println(dp[N][M]);
+    }
+}
