98e5ec794dec3494ed969b07c059f5be1677776e
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-07-29 19:35:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1912 ms, Memory: 190984 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll() + "\n");
+        }
+        System.out.println(sb);
+    }
+}
