27e51770cb8a3db9711bae954d9697c90d93fb93
JinguLeee <pingu514@naver.com>
2023-07-31 16:01:16 +0900

    [Silver I] Title: IOIOI, Time: 268 ms, Memory: 22016 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,22 +8,21 @@ public class Main {
         char[] chars = br.readLine().toCharArray();
 
         int count = 0;
-        List<Integer> countList = new ArrayList<>();
-        for (int i = 0; i <= M; i++) {
-            if (count == 0 && chars[i] != 'I') continue;
-
-            if (i != M && (count == 0 && chars[i] == 'I' || chars[i] != chars[i-1])) {
-                count++;
-            } else {
-                if (chars[(i == M ? i-1 : i)] != 'I') count--;
-                if (count >= 2*N+1) countList.add(count);
-                count = 1;
-            }
-        }
-
         int sum = 0;
-        for (Integer integer : countList) {
-            sum += (integer - (2*N+1)) / 2 + 1;
+        for (int i = 0; i < M - (2*N); i++) {
+            if (chars[i] != 'I') continue;
+
+            while (i < M - 2) {
+                if (chars[++i] == 'O' && chars[++i] == 'I') {
+                    count++;
+                } else {
+                    i--;
+                    break;
+                }
+            }
+
+            if (count - N + 1 > 0) sum += count - N + 1;
+            count = 0;
         }
         System.out.println(sum);
     }
