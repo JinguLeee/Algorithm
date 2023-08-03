1712ecb369bb46d63e9812810228ae9933e05fe4
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-08-03 18:36:18 +0900

    [Silver IV] Title: 제로, Time: 248 ms, Memory: 24000 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) stack.pop();
+            else stack.push(num);
+        }
+
+        int sum = 0;
+        for (Integer integer : stack) {
+            sum += integer;
+        }
+
+        System.out.println(sum);
+    }
+}
