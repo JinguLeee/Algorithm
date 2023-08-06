d3da8ceeee02289f8cee3dd1c2410ced1cfb80b7
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2023-08-06 11:43:25 +0900

    [Silver V] Title: 방 번호, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -3,17 +3,19 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String N = br.readLine();
+        int N = Integer.parseInt(br.readLine());
 
         int[] count = new int[9];
-        for (int i = 0; i < N.length(); i++) {
-            int num = Character.getNumericValue(N.charAt(i));
-            if (num == 9) count[6]++;
-            else count[num]++;
+
+        while (N > 0) {
+            int remain = N % 10;
+            if (remain == 9) count[6]++;
+            else count[remain]++;
+            N /= 10;
         }
         if (count[6] % 2 == 1) count[6]++;
         count[6] /= 2;
-
+        
         int max = 0;
         for (int i = 0; i < 9; i++) {
             max = Math.max(max, count[i]);
