c39e2415f16f2705e74be95a30d60bc76b272147
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2025-06-11 10:01:07 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,10 @@
+def dfs(start, path):
+    if len(path) == M:
+        print(*path)
+    else:
+        for i in range(start, N+1):
+            dfs(i, path+[i])
+
+
+N, M = map(int, input().split(' '))
+dfs(1, [])
