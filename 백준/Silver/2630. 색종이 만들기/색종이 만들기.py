c818135bcd1ca57eaf84baba1ac1e05b8f98546f
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2025-07-16 13:58:24 +0900

    [Silver II] Title: 색종이 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -6,27 +6,26 @@ def isPaper(x, y, size, paper) :
                 return False, -1
     return True, color
 
-def divide(x, y, size, paper, w, b) :
+def divide(x, y, size, paper) :
+    global w, b
     p, c = isPaper(x, y, size, paper)
     if p : 
         if c == 0: w += 1
         else: b += 1
     else:
         half = size//2
-        w, b = divide(x, y, half , paper, w, b)
-        w, b = divide(x+half, y, half , paper, w, b)
-        w, b = divide(x, y+half, half , paper, w, b)
-        w, b = divide(x+half, y+half, half , paper,w, b)
-    return w, b
+        divide(x, y, half , paper)
+        divide(x+half, y, half , paper)
+        divide(x, y+half, half , paper)
+        divide(x+half, y+half, half , paper)
 
 import sys
 si = sys.stdin.readline
 N = int(si())
 
-white = 0; blue = 0
-paper = [list(map(int, si().split())) for _ in range(N)]
 w = 0; b = 0
-w, b = divide(0, 0, N, paper, w, b)
+paper = [list(map(int, si().split())) for _ in range(N)]
+divide(0, 0, N, paper)
 
 print(w)
 print(b)
