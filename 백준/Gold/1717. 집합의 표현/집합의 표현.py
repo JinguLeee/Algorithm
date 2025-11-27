b2718ca7c453a7094af2b26fe940279557cc5651
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2025-11-27 15:35:42 +0900

    [Gold V] Title: 집합의 표현, Time: 308 ms, Memory: 72404 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,31 @@
+def find(num):
+    if sets[num] == num:
+        return num
+    sets[num] = find(sets[num])
+    return sets[num]
+
+
+def union(a, b):
+    if A == B:
+        return
+    elif A < B:
+        sets[A] = B
+    else:
+        sets[B] = A
+
+
+import sys
+
+si = sys.stdin.readline
+N, M = map(int, si().split())
+sets = [n for n in range(N + 1)]
+result = []
+for _ in range(M):
+    p, a, b = map(int, si().split())
+    A = find(a)
+    B = find(b)
+
+    if p == 0:  # 합집합
+        union(a, b)
+    else:  # 출력
+        print("YES" if A == B else "NO")
