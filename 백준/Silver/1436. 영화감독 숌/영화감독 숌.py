98727abc79f1aed2db5c5fd7bcc35fc7a66bbae2
JinguLeee <50609161+JinguLeee@users.noreply.github.com>
2025-12-08 15:01:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,9 +1,32 @@
 import sys
-si = sys.stdin.readline
-N = int(si())
 
-l = []
-for i in range(666, 2666800):
-    if '666' in str(i):
-        l.append(i)
-print(l[N-1])
+si = sys.stdin.readline
+N = int(si()) - 1
+
+idx = 0
+q = 0
+
+while True:
+    if q % 10 != 6:
+        if idx == N:
+            print(q * 1000 + 666)
+            break
+        idx += 1
+        q += 1
+        continue
+
+    cnt = 0
+    check_q = q
+    while check_q % 10 == 6:
+        cnt += 1
+        check_q //= 10
+
+    if idx + 10**cnt > N:
+        result = N - idx
+        result += 666 * (10**cnt)
+        result += check_q * (10 ** (cnt + 3))
+        print(result)
+        break
+    else:
+        idx += 10**cnt
+        q += 1
